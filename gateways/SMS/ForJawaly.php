a7f71235a4bb46dfe8f458d8233ce682a843cfcc
Mohamed Eid <medoeid50@gmail.com>
2021-10-30 16:53:30 +0200

    add 4jawaly to service provider

--- gateways/SMS/ForJawaly.php
+++ gateways/SMS/ForJawaly.php
@@ -21,15 +21,19 @@ class ForJawaly {
 	// Params  binding to Guzzel POST & GET
 	private $params = [];
 
+	// with method
+	private $method;
 
-    public function __construct()
-    {
-        $this->config = (object) config('gateways.sms.for_jawaly');
+	// Methods
+	private $methods = ['SEND', 'BALANCE'];
+
+    public function __construct(){
+        $this->config = (object) config('gateways.sms.4jawaly');
 
         $this->params['sender']   = $this->config->sender;
 		$this->params['username'] = $this->config->username;
 		$this->params['password'] = $this->config->password;
-		$this->params['unicode '] = 'E';
+		$this->params['unicode'] = 'E';
 		$this->params['return']   = 'json';
 
         $this->links = [
@@ -38,6 +42,64 @@ class ForJawaly {
 		];
         
     }
+
+
+		/**
+	 * @function checkPrams to check param exists
+	 * @return Exception
+	 */
+	protected function checkParams($params = [], $default = null) {
+		foreach ($params as $param) {
+			$param = strtolower($param);
+			if (is_null($default)) {
+				$all_keys = array_keys($this->params);
+				$all_keys = array_map('strtolower', $all_keys);
+
+				if (!in_array($param, $all_keys)) {
+					throw new \Exception($param . ' parameter not found');
+				}
+			} else {
+				$all_keys = array_keys($default);
+				$all_keys = array_map('strtolower', $all_keys);
+
+				if (!in_array($param, $all_keys, true)) {
+					throw new \Exception($param . ' parameter not found');
+				}
+			}
+		}
+	}
+
+	/**
+	 * function signature
+	 * to generate signature code for gateway
+	 * @return void
+	 */
+	protected function signature(): void {
+		if ($this->method == 'SEND') {
+			$params = ['sender', 'username', 'password', 'unicode', 'return', 'numbers', 'message'];
+		} elseif ($this->method == 'BALANCE') {
+			$params = ['sender', 'username', 'password', 'unicode', 'return'];
+		}
+
+		if (isset($params)) {
+			$this->checkParams($params);
+		}
+	}
+
+		/**
+	 * set the payment_method to binding in params
+	 * @return method chaining or Exception
+	 */
+	public function method($method): self {
+		if (in_array($method, $this->methods)) {
+			$this->params['method'] = $method;
+			$this->method = $method;
+			return $this;
+		} else {
+			// throw exception if not set payment method from array
+			throw new \Exception('Set payment Method ' . implode(',', $this->methods));
+		}
+	}
 
     public function mobile($mobile): self{
 		$this->params['numbers'] = is_array($mobile) ? implode(',', $mobile) : $mobile;
@@ -50,11 +112,15 @@ class ForJawaly {
 	}
 
     public function send(){
+		$this->signature();
+		unset($this->params['method']);
         $response = Http::get( $this->links['SEND'] ,$this->params);  
         return $response->json();
     }
     
 	public function balance() {
+		$this->signature();
+		unset($this->params['method'], $this->params['numbers'], $this->params['message']);
         $response = Http::get( $this->links['BALANCE'] ,$this->params);  
         return $response->json();
     }
